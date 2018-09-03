93151a0f1363ecc5d5c754af4a873873f40a4f96
Perrine <morandperrine@hotmail.fr>
2018-09-03 14:26:35 +0200

    Can I GET some POST

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,13 @@ router.get('/coucou-pug', function(req, res, next) {
   res.render('coucou', { title: 'Blop', sayHello: 'Hello buddy' });
 });
 
+router.get('/forms-:code(\\d+)?:level?', (req, res) => {
+  console.log(req.params.code);
+  console.log(req.query.level);
+});
+
+router.post('/forms-:code(\\d+)?:level?', (req, res) => {
+  console.log(req.body.name);
+});
+
 module.exports = router;
